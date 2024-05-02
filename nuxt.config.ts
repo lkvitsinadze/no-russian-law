d9380815f4f19b938d607835887035e6f450bb71
Levan Kvitsinadze <l.kvitsinadze@gmail.com>
2024-05-02 13:14:41 +0200

    Add UI packages and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    '@nuxt/content',
+    ['@nuxt/eslint', { fix: true }],
+    '@nuxt/ui',
+    '@nuxt/fonts'
+  ],
+  colorMode: {
+    preference: 'light'
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  ssr: false,
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
+</script>
+
+<template>
+  <UCard>
+    <template #header>
+      <Placeholder class="h-8" />
+    </template>
+
+    <Placeholder class="h-32" />
+
+    <template #footer>
+      <Placeholder class="h-8" />
+    </template>
+  </UCard>
+</template>
