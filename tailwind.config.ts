7b2c31da65158f68ef2b348e5582934217807cc4
Levan Kvitsinadze <l.kvitsinadze@gmail.com>
2024-05-03 19:39:32 +0200

    Add new fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,13 @@ export default <Partial<Config>>{
   theme: {
     extend: {
       fontFamily: {
-        sans: ['DM Sans', ...defaultTheme.fontFamily.sans]
-      }
+        sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+        "firago-regular": ["FiraGO-regular", "serif"],
+        "firago-medium": ["FiraGO-medium", "serif"],
+        "firago-bold": ["FiraGO-bold", "serif"],
+        "firago-light-italic": ["FiraGO-LightItalic", "serif"],
+        "firago-light": ["FiraGO-Light", "serif"],
+      },
     }
   }
 }
