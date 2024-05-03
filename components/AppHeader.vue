99173d4b56f3f6ec54f64f3458a6291799fafc2f
Ioseb Gvritishvili <soso@Iosebs-MacBook-Pro.local>
2024-05-03 13:09:36 +0400

    add protest page

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,27 +1,23 @@
 <script setup lang="ts">
 const links = [
-{
-  label: 'თანხის შეგროვება',
-  to: '#fundraising'
-},
+  {
+    label: 'არა რუსულ კანონს',
+    to: '/'
+  },
 // {
 //   label: 'უსაფრთხოების წესები',
 //   to: '#terms'
 // },
-{
-  label: 'იურიდიული დახმარება',
-  to: '#law-help'
-},
   {
-    label: 'საჭირო ინფორმაცია',
-    to: '#info'
+    label: 'აქცია',
+    to: '/protest'
   },
 ]
 </script>
 
 <template>
   <UHorizontalNavigation
-    :links="links"
-    class="border-b border-gray-200 dark:border-gray-800 flex-wrap"
+      :links="links"
+      class="border-b border-gray-200 dark:border-gray-800 flex-wrap"
   />
 </template>
