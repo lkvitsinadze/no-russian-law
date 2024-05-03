37eb33946c700f84081305c82c4586ef0b993f1d
Ioseb Gvritishvili <sosogvritishvili@gmail.com>
2024-05-03 13:14:14 +0400

    Feature/add protest page (#3)
    * add prostest page
    
    * add protest page
    
    ---------
    
    Co-authored-by: Ioseb Gvritishvili <soso@Iosebs-MacBook-Pro.local>

---
+++ pages/protest.vue
@@ -0,0 +1,53 @@
+<template>
+  <TheSection class="mb-4">
+
+    <ul class="mb-4">
+      <li>
+        <a href="#1" class="text-blue-500 underline">როგორ მოვიქცეთ აქციის დაშლის დროს</a>
+      </li>
+      <li>
+        <a href="#2" class="text-blue-500 underline">თუ პოლიციამ დაგაკავათ</a>
+      </li>
+    </ul>
+
+    <h2 class="font-bold text-xl my-4" id="1">
+      როგორ მოვიქცეთ აქციის დაშლის დროს
+    </h2>
+
+    <UCard class="max-w-[600px]">
+      <div class="fb-post"
+           data-href="https://www.facebook.com/khvedelidze.nika/posts/pfbid024vWM7L4edtNAyQNwN53MbdzcQSxU1XgRxFxp5GxBjVXNSyN1Asc3WifQVyBSReaVl"
+           data-show-metadata="false">
+      </div>
+    </UCard>
+
+    <h3 class="font-bold text-xl my-4" id="2">თუ პოლიციამ დაგაკავათ</h3>
+
+    <p class="pl-3 border-l-4 border-red-500 mb-4">
+      პოლიციელის მიერ თქვენი დაკავების შემთხვევაში, ეცადეთ გადაიღოთ დაკავების მიმდინარეობა დასაწყისიდან ბოლომდე. ვიდეო
+      მტკიცებულების ქონა ერთადერთი იარაღია თქვენს ხელში სიმართლის საჩვენებლად. უკეთესია, თუ ჩანაწერი ასახავს თქვენი
+      დაკავების გადაწყვეტილების მომენტსა და მანამდე სიტუაციას გარშემო. ეს დაგეხმარებათ გადაწყვეტილების უკანონობის
+      საჩვენებლად.
+      დაკავების შემდეგ, უფლება გაქვთ(!) ისარგებლოთ საკუთარი ტელეფონით, შეატყობინოთ დაკავების შესახებ ოჯახის წევრს ან
+      ნებისმიერ პირს (მაგალითად, გადაყვანის პროცესში). თუ პოლიცია გადაწყვეტს, რომ ჩამოგართვათ ტელეფონი და სხვა ნივთები,
+      მან აუცილებლად უნდა შეადგინოს ნივთების ჩამორთმევის შესახებ ოქმი.
+    </p>
+
+    <UCard class="max-w-[600px]">
+      <div class="fb-post"
+           data-href="https://www.facebook.com/shamemovement/posts/pfbid02vvafVB3sVTK9fwSf6NBStqY12AWxzHdqzFqHC5MbJ56hWFqhKpZ1EfkxvKLN4JQ8l"
+           data-show-metadata="false">
+      </div>
+    </UCard>
+  </TheSection>
+</template>
+
+<script>
+export default {
+  name: "protest"
+}
+</script>
+
+<style scoped>
+
+</style>
