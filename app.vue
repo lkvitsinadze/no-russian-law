fcd6268623dd2807e0c7656acf56b2254600cc79
lkvitsinadze <lkvitsinadze@users.noreply.github.com>
2024-05-03 14:50:46 +0200

    Feature/add base og (#4)
    * add prostest page
    
    * add protest page
    
    * new title and description added
    
    ---------
    
    Co-authored-by: Ioseb Gvritishvili <soso@Iosebs-MacBook-Pro.local>

--- app.vue
+++ app.vue
@@ -1,39 +1,44 @@
 <script setup lang="ts">
+
 useHead({
   meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    {name: 'viewport', content: 'width=device-width, initial-scale=1'}
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    {rel: 'icon', href: '/favicon.ico'}
   ],
   htmlAttrs: {
     lang: 'en'
   },
-  script: [ { innerHTML: `(function(d, s, id) {
+  script: [{
+    innerHTML: `(function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
             js = d.createElement(s);
             js.id = id;
             js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0&appId=1161289928451992&autoLogAppEvents=1';
             fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));` } ]
+        }(document, 'script', 'facebook-jssdk'));`
+  }]
 })
 
 useSeoMeta({
-  ogImage: 'https://landing-template.nuxt.dev/social-card.png',
-  twitterImage: 'https://landing-template.nuxt.dev/social-card.png',
+  ogTitle: 'პროტესტის გზამკვლელი - არა რუსულ კანონს',
+  ogDescription: 'არა რუსულ კანონს, მნიშვნელოვანი რესურსების ბაზა',
+  ogImage: '/fb-og-image.jpg',
+  twitterImage: '/fb-og-image.jpg',
   twitterCard: 'summary_large_image'
 })
 
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader/>
 
-  <NuxtPage />
+  <NuxtPage/>
 
-  <AppFooter />
+  <AppFooter/>
 
-  <UNotifications  />
+  <UNotifications/>
 
 </template>
